7676f89957ae923fa1c1290ec8c56d11a52b04ff
han1997 <hanhuiyuan1997@gmail.com>
2020-02-26 15:55:51 +0800

    gmall-user模块测试完成,项目几本分布式架构完成

---
+++ gmall-api/src/main/java/com/atguigu/gmall/service/IUserService.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall.service;
+
+import com.atguigu.gmall.bean.UmsMember;
+
+import java.util.List;
+
+/**
+ * @author hhy1997
+ * 2020/2/22
+ */
+public interface IUserService {
+    List<UmsMember> list();
+}
